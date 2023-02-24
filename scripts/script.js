d30d7d0c8426bff21ef81f0291df500503ffda23
Ekaterina Chaika <chaikacatherine@yandex.ru>
2023-02-25 02:51:49 +0300

    проект номер пять

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,59 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const popupBtnOpen = document.querySelector(".profile-info__button");
 const popupContainer = document.querySelector(".popup");
 const popupBtnClose = document.querySelector(".button-close");
+const popupProfile = document.querySelector(".popup__type_profile");
+const popupPlace = document.querySelector(".popup__type_place");
+const popupImg = document.querySelector(".popup__type_img");
+const popupBtnOpenPlace = document.querySelector(".add-button");
+const popupBtnClosePlace = document.querySelector(".button-close__type_place");
 
-popupBtnOpen.addEventListener("click", openPopup);
-popupBtnClose.addEventListener("click", closePopup);
+popupBtnOpen.addEventListener("click", () => {
+  openPopupProfile()
+});
 
-function openPopup() {
-  popupContainer.classList.add("popup_opened");
+popupBtnClose.addEventListener("click", () => {
+  closePopup(popupProfile)
+});
+
+function openPopupProfile() {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
+  openPopup(popupProfile);
 }
 
-function closePopup() {
-  popupContainer.classList.remove("popup_opened");
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
 }
 
 let formElement = document.querySelector(".form");
@@ -22,16 +63,80 @@ let jobInput = document.querySelector(".form__info_type_jobe");
 const profileTitle = document.querySelector(".profile-info__title");
 const profileSubtitle = document.querySelector(".profile-info__subtitle");
 
-function handleFormSubmit (evt) {
-    evt.preventDefault();
+function handleFormSubmit(evt) {
+  evt.preventDefault();
 
-    console.log(nameInput.value);
-    console.log(jobInput.value);
+  console.log(nameInput.value);
+  console.log(jobInput.value);
 
-    profileTitle.textContent = nameInput.value;
-    profileSubtitle.textContent = jobInput.value;
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
 
-    closePopup();
+  closePopup(popupProfile);
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+popupBtnOpenPlace.addEventListener("click", () => {
+  openPopup(popupPlace)
+});
+
+popupBtnClosePlace.addEventListener("click", () => {
+  closePopup(popupPlace)
+});
+
+const template = document.querySelector(".element-template").content.querySelector(".element");
+const elements = document.querySelector(".elements");
+const submitBtnPlace = document.querySelector(".button-submit__type_place");
+const namePlace = document.querySelector(".form__info_place_name");
+const linkPlace = document.querySelector(".form__info_img_link");
+
+function renderCards(initialCards) {
+  const cards = initialCards.map((item) => {
+    const card = template.cloneNode(true);
+    card.querySelector(".element__title").textContent = item.name;
+    card.querySelector(".element__image").src = item.link;
+    card.querySelector(".element__button-like").addEventListener("click", (evt) => {
+      evt.target.classList.toggle("element__button-like_active");
+    });
+    card.querySelector(".element__button-delete").addEventListener("click", () => {
+      card.remove();
+    });
+    return card;
+  });
+  elements.append(...cards);
+}
+
+renderCards(initialCards);
+
+submitBtnPlace.addEventListener("click", () => {
+  closePopup(popupPlace)
+});
+
+submitBtnPlace.addEventListener("click", (e) => {
+  e.preventDefault();
+  const title = namePlace.value;
+  const img = linkPlace.value;
+  const card = template.cloneNode(true);
+  card.querySelector(".element__title").textContent = title;
+  card.querySelector(".element__image").src = img;
+  card.querySelector('.element__image').alt = title;
+  card.querySelector(".element__button-like").addEventListener("click", (evt) => {
+    evt.target.classList.toggle("element__button-like_active");
+  });
+  elements.prepend(card);
+});
+
+const popupImage = document.querySelector(".popup__image");
+const popupImageTitle = document.querySelector(".popup__image-title");
+const popupBtnCloseImage = document.querySelector(".button-close__type_img");
+
+popupBtnCloseImage.addEventListener("click", () => {
+  closePopup(popupImg);
+});
+
+elements.addEventListener("click", function createPopupImage(evt) {
+  popupImage.src = evt.target.closest(".element__image").src;
+  popupImageTitle.textContent = evt.target.closest(".element__image").src;
+  openPopup(popupImg);
+});
